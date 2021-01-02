27d2e3cd0f3e7609c56bf4ddf94f76c856221d07
RaktiSingal <raktisingal83@gmail.com>
2021-01-02 22:45:52 +0530

    Perimeter of Rectangle

---
+++ 07-program_to_calculate_perimeter_of_rectangle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main  
+ {  
+   public static void main (String args[])  
+    {      float a ,b, c, d, perimeter;
+    Scanner sc=new Scanner(System.in);
+                    a=sc.nextInt();
+                    b=sc.nextInt();
+                    perimeter  = 2*(a+b);  
+            System.out.println("Perimeter of Rectangle is: "+perimeter);  
+    }}  
